0e65febfe9b100f324bc87da806fbe72016ab343
Guilherme Pereira <pgguilherme337@gmail.com>
2024-06-22 16:11:03 -0300

    fix: try to fix bug ThemeContext

--- src/common/ThemeContext.jsx
+++ src/common/ThemeContext.jsx
@@ -1,22 +1,25 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
+// Cria o contexto do tema
 const ThemeContext = createContext();
 
-export const useTheme = () => useContext(ThemeContext);
-
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(
-    () => localStorage.getItem('theme') || 'dark'
-  );
-
-  useEffect(() => {
-    document.body.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-  }, [theme]);
+  const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
@@ -24,3 +27,5 @@ export const ThemeProvider = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
+
+export const useTheme = () => useContext(ThemeContext);
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/guigas-icon.jpg';
 import sun from '../../assets/sun.svg';
@@ -8,56 +9,56 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/curriculo.pdf'
+import CV from '../../assets/curriculo.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
-    const { theme, toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
+  console.log(`Current theme: ${theme}`);
 
-    const themeIcon = theme === 'light' ? sun : moon;
-    const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
-    const githubIcon = theme === 'light' ? githubLight : githubDark;
-    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
   return (
     <section id='hero' className={styles.container}>
-        <div className={styles.colorModeContainer}>
-            <img 
-                className={styles.hero} 
-                src={heroImg}
-                alt="Profile picture of Guilherme" 
-            />
-            <img
-                className={styles.colorMode}
-                src={themeIcon}
-                alt="Color mode icon"
-                onClick={toggleTheme}
-             />
-        </div>
-        <div className={styles.info}>
-            <h1>
-                Guilherme
-                <br /> 
-                Pereira
-            </h1>
-            <h2>Software Developer</h2>
-            <span>
-                <a href="https://twitter.com/guigasdev" target='_blank'><img src={twitterIcon} alt="Twitter Icon" /></a>
-                <a href="https://github.com/guigasdev" target='_blank'><img src={githubIcon} alt="Github Icon" /></a>
-                <a href="https://www.linkedin.com/in/guilherme-pereiradev/" target='_blank'><img src={linkedinIcon} alt="Linkedin Icon" /></a>
-                
-            </span>
-            <p className={styles.description}>
-                Tenho paixão por programação e tecnologia, sobretudo, desenvolvimento backend, espero que possa te ajudar, entre em contato comigo!
-            </p>
-            <a href={CV} download>
-                <button className='hover'>
-                    Currículo
-                </button>
-            </a>
-        </div>
+      <div className={styles.colorModeContainer}>
+        <img 
+          className={styles.hero} 
+          src={heroImg}
+          alt="Profile picture of Guilherme" 
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Guilherme
+          <br /> 
+          Pereira
+        </h1>
+        <h2>Software Developer</h2>
+        <span>
+          <a href="https://twitter.com/guigasdev" target='_blank'><img src={twitterIcon} alt="Twitter Icon" /></a>
+          <a href="https://github.com/guigasdev" target='_blank'><img src={githubIcon} alt="Github Icon" /></a>
+          <a href="https://www.linkedin.com/in/guilherme-pereiradev/" target='_blank'><img src={linkedinIcon} alt="Linkedin Icon" /></a>
+        </span>
+        <p className={styles.description}>
+          Tenho paixão por programação e tecnologia, sobretudo, desenvolvimento backend, espero que possa te ajudar, entre em contato comigo!
+        </p>
+        <a href={CV} download>
+          <button className='hover'>
+            Currículo
+          </button>
+        </a>
+      </div>
     </section>
-  ) 
+  ); 
 }
 
-export default Hero
+export default Hero;
